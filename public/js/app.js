cd94bed77b8bc7bb406521a8ff67f35386697c10
cressyfrost <cressyfrost@hotmail.com>
2018-07-11 02:35:20 +0700

    Major UI redesign

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,17 @@ var Crawl;
 function myTimer() {
     var d = new Date();
     var t = d.toLocaleTimeString();
-    $("#response").append( "<br>"+t );
+    var elem = document.getElementById('responses');
+    elem.scrollHeight = elem.scrollHeight+5;
+    $("#responses").append( '<div>'+t+'</div>' );
+    var elem = document.getElementById('responses');
+  	elem.scrollTop = elem.scrollHeight;
+    console.log(elem);
     //document.getElementById("response").innerHTML = t;
 } 
 
 function BeginLoop() {
-	Crawl = setInterval(myTimer, 1000);
+	Crawl = setInterval(myTimer, 100);
 }
 
 function EndLoop() {
