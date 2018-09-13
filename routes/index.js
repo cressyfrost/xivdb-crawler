6e7d65eb4e0f55d23d6e6558e49f1fabe4dea99c
Cress <cressyfrost@hotmail.com>
2018-09-13 18:46:46 +0700

    Added item endpoint

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'XIVDB Crawler' });
 });
 
+/* AJAX xivdb item GET */
+router.get('/item/:itemId', function (req, res) {
+
+xivdb.getItem(req.params.itemId, function(err, data){ 
+        if(err) return res.send(err);       
+        res.send(data);
+    });
+
+})
+
+
+
+
 module.exports = router;
--- xivdb.js
+++ xivdb.js
@@ -1,48 +1,34 @@
 module.exports = {
-  xivdb: function() {
+  getItem: function(id,callback) {
     var http = require('http');
     const chalk = require('chalk');
+    let itemId = id;
 
     var options = {
       host: 'api.xivdb.com'
     };
 
-    var pool = [];
-
-    for(var x=1; x<5; x++)
-    {
-        pool.push('/item/'+x); 
-    }
-
-    //console.log(pool);
-
-    var index = 0;
-    //request(100).setInterval(2000);
-    var refreshIntervalId = setInterval(function request() {
-      options.path = pool[index++];
-      http.get(options, function(res){
-        var body = '';
-        res.on('data', function(chunk){
-           //console.log(x++);
-           body += chunk;
-           //console.log(chalk.green(options.path));
-           //console.log(chunk.toString('utf8'));
-        });
-
-        res.on('end', function(){
-               var fbResponse = JSON.parse(body);
-               console.log(chalk.green("Fetched: "), fbResponse.name);
-               if(index >= pool.length)
-                {
-                  clearInterval(refreshIntervalId);
-                  console.log(chalk.blue("Done. Fetched "+index+" items."));
-                }
-           });
-
-      }).on("error", function(e){
-        console.log("Got error: " + e.message);
+    options.path = '/item/'+itemId;
+    http.get(options, function(res){
+      var body = '';
+      res.on('data', function(chunk){
+         //console.log(x++);
+         body += chunk;
+         //console.log(chalk.green(options.path));
+         //console.log(chunk.toString('utf8'));
       });
-    }
-    , 2000);
+
+      res.on('end', function(){
+             var fbResponse = JSON.parse(body);
+             console.log(chalk.green("Fetched: "), fbResponse.name);
+             return callback(fbResponse, false);
+         });
+
+    }).on("error", function(e){
+      console.log("Got error: " + e.message);
+      return callback(null, error);;
+    });
+    
+
   }
 }
